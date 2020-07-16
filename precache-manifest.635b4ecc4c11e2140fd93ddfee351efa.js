750653c43752d96f5e7e56ae0a58f259612087fb
juan camilo acevedo <juancaacevedo10@hotmail.com>
2020-07-16 00:48:43 -0500

    Updates

---
+++ precache-manifest.635b4ecc4c11e2140fd93ddfee351efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a8d9f1125e0059d7946460d109c0f7",
+    "url": "/social-media-dashboard/index.html"
+  },
+  {
+    "revision": "baa7d39f0636cfe1d2c2",
+    "url": "/social-media-dashboard/static/css/main.263076c1.chunk.css"
+  },
+  {
+    "revision": "0040656ae50036dfcab0",
+    "url": "/social-media-dashboard/static/js/2.27091492.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-dashboard/static/js/2.27091492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa7d39f0636cfe1d2c2",
+    "url": "/social-media-dashboard/static/js/main.b064578c.chunk.js"
+  },
+  {
+    "revision": "3b1ddb8a90a9326f6c18",
+    "url": "/social-media-dashboard/static/js/runtime-main.9d9f00af.js"
+  }
+]);
